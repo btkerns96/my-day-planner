2e7a5e8dc4082e7aa3cf4ef424029ac33606eb77
BrandonKerns <btkerns96@gmail.com>
2023-04-26 11:50:08 -0700

    Added local storage function

--- script.js
+++ script.js
@@ -19,4 +19,31 @@ $(document).ready(function () {
         $(this).addClass('future');
       }
     });
-  }});
+  }
+
+  function loadSavedData() {
+    // Get any user input that was saved in localStorage and set the values of the corresponding textarea elements
+    $('.time-block').each(function () {
+      const id = $(this).attr('id');
+      const savedDescription = localStorage.getItem(id);
+
+      if (savedDescription) {
+        $(this).find('textarea').val(savedDescription);
+      }
+    });
+  }
+
+  // Add a listener for click events on the save button
+  $('.saveBtn').on('click', function () {
+    // Get the id of the time-block containing the clicked button
+    const timeBlockId = $(this).closest('.time-block').attr('id');
+
+    // Save the user input in localStorage using the id as a key
+    const description = $(this).siblings('textarea').val();
+    localStorage.setItem(timeBlockId, description);
+  });
+
+  // Initialize the page
+  applyTimeClass();
+  loadSavedData();
+});
